9f016ad7d433639ebd0dc438b513f5323b258c69
Ge Xu <michaelx@GematoMacBook-Pro.local>
2019-12-06 22:01:32 -0600

    luck plzzzzzzzz

--- src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
+++ src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
@@ -158,7 +158,7 @@ public class SimpleDNS
 		List<DNSResourceRecord> additions = dns.getAdditional();
 		for(DNSResourceRecord auth_entry: auths){
 			for(DNSResourceRecord add_entry: additions){
-				String auth_string = new String(auth_entry.getData().serialize());
+				String auth_string = auth_entry.getData().toString();
 				System.out.println("processing auth:"+auth_string+"     addtional:"+add_entry.getName());
 				String add_string = new String(add_entry.getData().serialize());
 				if(auth_entry.getType()==DNS.TYPE_NS && 
