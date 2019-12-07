b3ce71ef3094a828bb4d14bcffa80fb89ea1aa96
Ge Xu <michaelx@GematoMacBook-Pro.local>
2019-12-06 23:28:32 -0600

    luck plzzzzzzzz

--- src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
+++ src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
@@ -218,6 +218,7 @@ public class SimpleDNS
 				InetAddress ip = InetAddress.getByName(record.getData().toString());
 				String ipStr = ip.toString();
 				String[] ips = ipStr.split(".");
+				System.out.println(ipStr);
 				int ipInt = Integer.parseInt(ips[0])*256*256*256+
 				Integer.parseInt(ips[1])*256*256+
 				Integer.parseInt(ips[2])*256+ Integer.parseInt(ips[3]);
