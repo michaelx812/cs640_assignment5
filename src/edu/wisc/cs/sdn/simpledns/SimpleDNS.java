18602ff3507249c4bc45effb4ead10975ce02cf0
Ge Xu <michaelx@GematoMacBook-Pro.local>
2019-12-07 16:38:55 -0600

    test recur

--- src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
+++ src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
@@ -394,7 +394,7 @@ public class SimpleDNS
 					String entry_Ip_str = entry.ip;
 					int entry_Ip_Int = string_to_ip(entry_Ip_str);
 					int mask = 0xffffffff << (32-entry.mask);
-					System.out.println("non comparing "+record_ip_Str+"   to   "+entry_Ip_Int&mask);
+					System.out.println("non comparing "+record_ip_Str+"   to   "+(entry_Ip_Int&mask));
 					if((record_ip_Int & mask) == (entry_Ip_Int&mask)){
 						System.out.println("FInd + "+entry_Ip_str);
 						DNSRdata data = new DNSRdataString(entry.location + "-" + record_ip_Str);
